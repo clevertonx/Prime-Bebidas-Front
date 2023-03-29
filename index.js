d98a593db3ab4fa9d4183729803df9598558f449
clevertonx <clevertonx@gmail.com>
2023-03-29 17:54:07 -0400

    hamburguer pifio

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-window.addEventListener("scroll", function() {
-    var header = document.getElementById("header");
-    if (window.pageYOffset > 0) {
-      header.classList.add("small");
-    } else {
-      header.classList.remove("small");
-    }
-  });
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+  if (window.pageYOffset > 0) {
+    header.classList.add("small");
+  } else {
+    header.classList.remove("small");
+  }
+});
+
+function menuShow() {
+  let menuMobile = document.querySelector('.menuMobile');
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+  } else {
+    menuMobile.classList.add('open')
+  }
+}
