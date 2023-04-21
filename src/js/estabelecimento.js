8588cf2694b63777d19d5192f1a6959d829b0e61
Sérgio Augusto David Júnior <sergio.adjr@gmail.com>
2023-04-21 10:16:50 -0400

    meus estabelecimentos iniciado - falta arrumar o js

---
+++ src/js/estabelecimento.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('#form-estabelecimento')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(form)
+
+    axios.post('http://localhost:8080/usuario/estabelecimento', {
+        nome: formData.get('nome'),
+        telefone: formData.get('telefone'),
+        horarioAtendimento: formData.get('horarioAtendimento'),
+        numero: formData.get('numero'),
+        cidade: formData.get('cidade'),
+        logradouro: formData.get('logradouro'),
+        estado: formData.get('estado'),
+        cnpj: formData.get('cnpj'),
+        usuario: {
+            email: 'email_do_usuario',
+            senha: 'senha_do_usuario'
+        }
+    }).then(response => {
+        console.log('Estabelecimento cadastrado com sucesso:', response.data)
+        alert('Estabelecimento cadastrado com sucesso!')
+    }).catch(error => {
+        console.error('Erro ao cadastrar estabelecimento:', error)
+        alert('Erro ao cadastrar estabelecimento!')
+    })
+})
