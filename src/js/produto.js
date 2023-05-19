ce7d0d611f566cca0cb28f8f9c1628f95ead39d4
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-05-19 17:36:09 -0400

    att

--- src/js/produto.js
+++ src/js/produto.js
@@ -63,9 +63,55 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-document.getElementById("logout").addEventListener(() => {
-    document.cookie = 'idUsuario=';
-})
+
+
+
+function obterDadosEstabelecimentos() {
+    axios.get('http://localhost:8080/estabelecimento', {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(function (response) {
+            // Dados obtidos com sucesso
+            const estabelecimentos = response.data;
+
+            // Chama a função para popular o select
+            popularSelect(estabelecimentos);
+        })
+        .catch(function (error) {
+            // Ocorreu um erro ao obter os dados da API
+            console.log(error);
+        });
+}
+
+
+function popularSelect(estabelecimentos) {
+    const selectElement = document.getElementById('estabelecimentos-cadastrados');
+
+    // Limpa o conteúdo atual do select
+    selectElement.innerHTML = '';
+
+    // Cria a opção padrão
+    const defaultOption = document.createElement('option');
+    defaultOption.text = 'Selecione um estabelecimento';
+    defaultOption.value = '';
+    selectElement.add(defaultOption);
+
+    // Percorre os estabelecimentos e cria as opções do select
+    for (var i = 0; i < estabelecimentos.length; i++) {
+        const estabelecimento = estabelecimentos[i];
+
+        const option = document.createElement('option');
+        option.text = estabelecimento.nome;
+        option.value = estabelecimento.idEstabelecimento;
+        selectElement.add(option);
+    }
+}
+
+window.onload = function () {
+    obterDadosEstabelecimentos();
+};
 
 
 
@@ -73,23 +119,8 @@ document.getElementById("logout").addEventListener(() => {
 
 
 
-// talvez esteja errado
-// function exibirCard(idProduto, idEstabelecimento) {
-//     axios.get(`/produto/${idProduto}/estabelecimento/${idEstabelecimento}`)
-//         .then(response => {
-//             // Atualiza o HTML do card com os dados do produto
-//             document.getElementById("nome").innerText = response.data.nome;
-//             document.getElementById("descricao").innerText = response.data.descricao;
-//             document.getElementById("preco").innerText = response.data.valor;
-//             document.getElementById("categoria").innerText = response.data.valor;
-//             document.getElementById("marca").innerText = response.data.valor;
-//             document.getElementById("imagem").innerText = response.data.valor;
-//             document.getElementById("estabelecimento").innerText = response.data.valor;
-//         })
-//         .catch(error => {
-//             console.error(error);
-//         });
-// }
+
+
 
 
 document.getElementById("logout").addEventListener.addEventListener('click', () => {
