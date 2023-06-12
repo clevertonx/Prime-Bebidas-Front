dc45461e4bf303ed4de124fbadfb8d51855d9132
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-06-12 12:51:11 -0400

    Cards categoria dinamicamente

--- src/js/cards-categorias.js
+++ src/js/cards-categorias.js
@@ -1,35 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const categorias = document.getElementById("categorias");
+function buscarProdutosPorCategoria(categoria) {
     const cardsContainer = document.getElementById("cards-container");
+    axios.get(`http://localhost:8080/produto/categoria?categoria=${categoria}`)
+        .then(response => {
+            const produtos = response.data;
 
-    categorias.addEventListener("click", function (event) {
-        event.preventDefault();
-        const categoria = event.target.dataset.categoria;
-        if (categoria) {
-            buscarProdutosPorCategoria(categoria);
-        }
-    });
+            cardsContainer.innerHTML = ""; // Limpa o conteúdo existente
 
-    function buscarProdutosPorCategoria(categoria) {
-        fetch(`/produto/categoria?categoria=${categoria}`)
-            .then(response => response.json())
-            .then(produtos => {
-                cardsContainer.innerHTML = ""; // Limpa o conteúdo existente
-
-                produtos.forEach(produto => {
-                    const card = `
-                        <div class="card-grande">
-                            <img src="${produto.imagem}" alt="${produto.nome}">
-                            <h4>${produto.nome}</h4>
-                            <p class="preco">$${produto.preco}</p>
-                            <p class="descricao">${produto.descricao}</p>
-                        </div>
-                    `;
-                    cardsContainer.insertAdjacentHTML("beforeend", card);
-                });
-            })
-            .catch(error => {
-                console.error("Erro ao buscar produtos:", error);
+            produtos.forEach(produto => {
+                const card = `
+                    <div class="card-grande">
+                        <img src="${produto.imagem}" alt="${produto.nome}">
+                        <h4>${produto.nome}</h4>
+                        <p class="preco">$${produto.preco}</p>
+                        <p class="descricao">${produto.descricao}</p>
+                    </div>
+                `;
+                cardsContainer.insertAdjacentHTML("beforeend", card);
             });
-    }
-});
+        })
+        .catch(error => {
+            console.error("Erro ao buscar produtos:", error);
+        });
+}
