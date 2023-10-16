fae65694041481c6544dc1e3664035f81faaedce
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-10-16 11:59:54 -0400

    .

--- src/js/login.js
+++ src/js/login.js
@@ -9,34 +9,39 @@ document.addEventListener('DOMContentLoaded', function () {
         const email = emailInput.value;
         const senha = senhaInput.value;
 
-
         axios.post('http://localhost:8080/usuario/login', { email, senha }, {
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-            .then(response => {
-                const tokenJWT = response.data.tokenJWT;
+        .then(response => {
+            const tokenJWT = response.data.tokenJWT;
 
-                document.cookie = `token=${tokenJWT}; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/`;
-                
-                if (response.data && response.data.id && response.data.tokenJWT) {
-                    document.cookie = 'idUsuario=' + response.data.id;
-                    alert("entrei");
-                } else {
-                    console.log("Resposta inválida do servidor após o cadastro do usuário");
-                }
-                
-                document.cookie = 'idUsuario=' + response.data.id;
-                window.location.href = 'logado.html'; // Movido aqui, mas você pode optar por mantê-lo dentro do bloco if
-            })
-            .catch(error => {
-                console.error(error);
-                alert("Usuário ou senha inválidos.")
-            });
+            // Extrai o ID do usuário do payload
+            const payloadBase64 = tokenJWT.split('.')[1];
+            const payloadString = atob(payloadBase64);
+            const payloadJSON = JSON.parse(payloadString);
+            const idUsuario = payloadJSON.id;
+
+            document.cookie = `token=${tokenJWT}; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/`;
+
+            if (idUsuario) {
+                document.cookie = `idUsuario=${idUsuario}; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/`;
+                alert("entrei");
+            } else {
+                console.log("Resposta inválida do servidor após o cadastro do usuário");
+            }
+
+            window.location.href = 'logado.html';
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Usuário ou senha inválidos.")
+        });
     });
 });
 
-document.getElementById("logout").addEventListener.addEventListener('click', () => {
-    document.cookie = 'idUsuario=';
+document.getElementById("logout").addEventListener('click', () => {
+    document.cookie = 'idUsuario=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 });
