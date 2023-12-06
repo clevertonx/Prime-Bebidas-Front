eeb8b2f24c207ea60ad1f980dd2db08f0e9c129e
clevertonx <clevertonx@gmail.com>
2023-12-05 21:30:16 -0400

    ResetPasswordTeste

--- src/js/redirectToResetPassword.js
+++ src/js/redirectToResetPassword.js
@@ -1,2 +1,29 @@
 const token = new URLSearchParams(window.location.search).get('token');
 document.getElementById('tokenInput').value = token;
+console.log(value);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const senha = document.getElementById('nova-senha');
+    const botaoRedefinirSenha = document.getElementById('redefinir-nova-senha');
+
+    botaoRedefinirSenha.addEventListener('click', function (event) {
+        event.preventDefault();
+        const senhaNova = senha.value;
+
+        axios.post('http://localhost:8080/register/reset-password/'+ {token} , { senhaNova }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            console.log("Solicitação de redefinição de senha enviada com sucesso!");
+            alert("Em breve você receberá um e-mail com um link para resetar a sua senha");
+            senhaNova.style.display = 'none'; 
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Houve um erro ao processar sua solicitação. Tente novamente mais tarde.");
+        });
+    });
+});
