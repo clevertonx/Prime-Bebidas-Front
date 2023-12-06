f0419098cfbc6900512c89210c7e22e488c06c83
clevertonx <clevertonx@gmail.com>
2023-12-05 23:14:59 -0400

    ResetSenhaTest

--- src/js/redirectToResetPassword.js
+++ src/js/redirectToResetPassword.js
@@ -1,7 +1,5 @@
-const token = new URLSearchParams(window.location.search).get('token');
 document.getElementById('tokenInput').value = token;
 console.log(value);
-
 
 document.addEventListener('DOMContentLoaded', function () {
     const senha = document.getElementById('nova-senha');
@@ -11,19 +9,25 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         const senhaNova = senha.value;
 
-        axios.post('http://localhost:8080/register/reset-password/'+ {token} , { senhaNova }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            console.log("Solicitação de redefinição de senha enviada com sucesso!");
-            alert("Em breve você receberá um e-mail com um link para resetar a sua senha");
-            senhaNova.style.display = 'none'; 
-        })
-        .catch(error => {
-            console.error(error);
-            alert("Houve um erro ao processar sua solicitação. Tente novamente mais tarde.");
-        });
+        const formData = new URLSearchParams();
+        formData.append('token', token);
+        formData.append('senhaNova', senhaNova);
+
+        axios.post('http://localhost:8080/register/reset-password',
+            formData
+            , {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                console.log("Solicitação de redefinição de senha enviada com sucesso!");
+                alert("Em breve você receberá um e-mail com um link para resetar a sua senha");
+                senha.style.display = 'none';
+            })
+            .catch(error => {
+                console.error(error);
+                alert("Houve um erro ao processar sua solicitação. Tente novamente mais tarde.");
+            });
     });
 });
